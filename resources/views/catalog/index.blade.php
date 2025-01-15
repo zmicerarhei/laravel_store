0e426b199c1b0206a5c14df9ca083816ce986ff2
Dzmitry Arhei <demetris167@gmail.com>
2025-01-16 00:20:19 +0300

    update layout

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -19,7 +19,29 @@
                 <div class="col">
                     <div class="sorting_bar d-flex flex-md-row flex-column align-items-md-center justify-content-md-start">
                         <div class="results">Найдено <span>{{ $total }}</span> результатов</div>
-                        <div class="sorting_container ml-md-auto">
+                        <div class="sorting_container d-flex ml-md-auto">
+                            <div class="currencies">
+                                <ul class="item_sorting">
+                                    <li>
+                                        <span class="sorting_text">Выберите валюту</span>
+                                        <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                                        <ul>
+                                            <li class="product_sorting_btn" data-order='default'>
+                                                <span>Беларуский рубль, BYN</span>
+                                            </li>
+                                            <li class="product_sorting_btn" data-order='price-low-high'>
+                                                <span>Доллар США, USD</span>
+                                            </li>
+                                            <li class="product_sorting_btn" data-order='price-high-low'>
+                                                <span>Евро, EUR</span>
+                                            </li>
+                                            <li class="product_sorting_btn" data-order='name-a-z'>
+                                                <span>Российский рубль, RUB</span>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
                             <div class="sorting">
                                 <ul class="item_sorting">
                                     <li>
