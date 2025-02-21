084f79bac8e9fc2d8f3feb3a5d104d7cf262234e
Dzmitry Arhei <demetris167@gmail.com>
2025-02-21 18:37:13 +0300

    small bugfix, add strict types

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use App\Contracts\RegisterServiceInterface;
-use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Services\RegisterService;
 
 class IsAdmin
 {
